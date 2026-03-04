a34d7c8e9b3f730428895290163ba4f5cb051ee0
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-03-04 17:51:54 +1000

    Add a new favicon, change the import for tailwind and update the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App () {
+  const [enabled, setEnabled] = useState(false)
+
   useEffect(() => {
-    console.log('Efecto')
-  })
+    console.log('Efecto ', { enabled })
+  }, [enabled])
 
   return (
-    <h3>Follow me!</h3>
+    <main>
+      <div className='w-12.5 h-12.5 absolute bg-[#09f] rounded-full opacity-[0.8] pointer-events-none -left-6.25 -top-6.25 translate-x-0 translate-y-0'></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Stop following ' : 'Start following '} me!
+      </button>
+    </main>
   )
 }
 
